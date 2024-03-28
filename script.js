0aa5200c9c5277aaaeddd58cf67226cf6eb12138
robkellems <robertgkellems@gmail.com>
2024-03-27 23:28:34 -0400

    added keyboard shortcuts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 const message = document.querySelector('.message');
+const keypadMap = new Map([[8,-30],[9,-18],[6,3],[3,22],[2,30],[1,18],[4,-3],[7,-22]]);
 let currentNumber = 2;
 let currentSquare;
 let gameOver = false;
@@ -51,40 +52,50 @@ function highlightValidMoves(index) {
 
 function registerMove(index) {
     let moveSquare = grid.children[index];
-    // Set attributes for new clicked square
-    moveSquare.textContent = currentNumber;
-    moveSquare.classList.add('filled', 'current', 'animate');
-    moveSquare.classList.remove('highlight');
-    document.querySelectorAll('.highlight').forEach(square => square.classList.remove('highlight'));
 
-    // Change previously clicked square, reset variables for new square
-    currentSquare.classList.remove('current');
-    currentSquare = moveSquare;
-    currentNumber++;
+    if (!gameOver && moveSquare.classList.contains('highlight')) {
+        // Set attributes for new clicked square
+        moveSquare.textContent = currentNumber;
+        moveSquare.classList.add('filled', 'current', 'animate');
+        moveSquare.classList.remove('highlight');
+        document.querySelectorAll('.highlight').forEach(square => square.classList.remove('highlight'));
 
-    if (currentNumber > 100) {
-        const winningSound = new Audio("resources/crowdcheer.ogg");
-        winningSound.play();
-        message.textContent = 'Congratulations! You won!';
-        gameOver = true;
-    } else {
-        highlightValidMoves(moveSquare.dataset.index);
-        if (document.querySelectorAll('.highlight').length === 0) {
-            const gameOverSound = new Audio("resources/EMEXTR4.wav");
-            gameOverSound.play();
-            message.textContent = 'Game Over! Your score: ' + (currentNumber - 1);
+        // Change previously clicked square, reset variables for new square
+        currentSquare.classList.remove('current');
+        currentSquare = moveSquare;
+        currentNumber++;
+
+        if (currentNumber > 100) {
+            const winningSound = new Audio("resources/crowdcheer.ogg");
+            winningSound.play();
+            message.textContent = 'Congratulations! You won!';
             gameOver = true;
-        }
+        } 
         else {
-            const clickSound = new Audio("resources/EMCLK13.wav");
-            clickSound.play();
+            highlightValidMoves(moveSquare.dataset.index);
+            if (document.querySelectorAll('.highlight').length === 0) {
+                const gameOverSound = new Audio("resources/EMEXTR4.wav");
+                gameOverSound.play();
+                message.textContent = 'Game Over! Your score: ' + (currentNumber - 1);
+                gameOver = true;
+            }
+            else {
+                const clickSound = new Audio("resources/EMCLK13.wav");
+                clickSound.play();
+            }
         }
     }
 }
 
 function handleClick(e) {
-    if (!gameOver && e.target.classList.contains('highlight')) {
-        registerMove(e.target.dataset.index);
+    registerMove(e.target.dataset.index);
+}
+
+function handleKeyPress(e) {
+    let numPressed = e.keyCode - 48;
+    if (keypadMap.has(numPressed)) {
+        let moveIndex = parseInt(currentSquare.dataset.index) + keypadMap.get(numPressed);
+        if (moveIndex > -1 && moveIndex < 100) registerMove(moveIndex);
     }
 }
 
@@ -92,3 +103,4 @@ createGrid();
 placeNumber1();
 highlightValidMoves(document.querySelector('.filled').dataset.index);
 grid.addEventListener('click', handleClick);
+this.addEventListener('keypress', handleKeyPress);
